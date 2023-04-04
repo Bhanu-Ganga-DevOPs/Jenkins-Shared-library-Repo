b6e59d84f9adc08885be8df5f6eabed8e64ba59c
Bhanu-Ganga-DevOPs <gangabhavanikatraparthi@gmil.com>
2023-04-04 04:45:18 +0000

    Parameterised git checkout

--- vars/gitCheckout.groovy
+++ vars/gitCheckout.groovy
@@ -1,1 +1,8 @@
-#!bin/bash
+def call(Map stageParams){
+    checkout([
+        $class: 'GitSCM',
+        branches: [[name: stageParams.branch]],
+        userRemoteConfigs: [[url: stageParams.url]]
+        credentialsId :[[credentialsId : stageParams.credentialsId ]]
+    ])
+}
