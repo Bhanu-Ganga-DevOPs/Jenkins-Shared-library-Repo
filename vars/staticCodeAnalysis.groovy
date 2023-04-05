fa4d291b4b16d9a18ff9029a110c0b42122d182d
Ubuntu <ubuntu@ip-172-31-16-199.ec2.internal>
2023-04-05 03:32:30 +0000

    added autgentiacation to sonarqube

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -1,4 +1,7 @@
-def call(){
+def call(credentialsId){
 
-    sh 'mvn clean package sonar:sonar'
+    withSonarQubeEnv(credentialsId: credentialsId) {
+        sh 'mvn clean package sonar:sonar'
+    }
+    
 }
