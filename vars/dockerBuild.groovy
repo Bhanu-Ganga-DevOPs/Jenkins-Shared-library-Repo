b18d47d35dcec0fa1f033d7c00114b3aa29399a9
Bhanu-Ganga-DevOPs <gangabhavanikatraparthi@gmail.com>
2023-04-05 04:52:42 +0000

    dockergroovy  added

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,10 +1,19 @@
 
-def call(string project, string ImageTag,string hubUser){
+// def call(String project, String ImageTag, String hubUser){
 
-    sh """ 
-    docker image build -t ${hubUser}/${project} .
-    docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
-    docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+//     sh """ 
+//     docker image build -t ${hubUser}/${project} .
+//     docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
+//     docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+//     """
+
+// }
+
+def call(String project, String ImageTag, String hubUser){
+    
+    sh """
+     docker image build -t ${hubUser}/${project} . 
+     docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
+     docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
     """
-
 }
