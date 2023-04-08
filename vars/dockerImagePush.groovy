15a328143bdc0651f0f645f9dceb49c756a5b370
Bhanu-Ganga-DevOPs <gangabhavanikatraparthi@gmail.com>
2023-04-08 06:57:14 +0000

    groovy changed

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -15,13 +15,20 @@
 
 
  def call(String aws_account_id, String region, String ecr_repoName){
+ 
+	withCredentials([<object of type com.cloudbees.jenkins.plugins.awscredentials.AmazonWebServicesCredentialsBinding>]) {
     
-    sh """
+		 sh """
 
-     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+		 aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
     
-   docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
-    """
+		docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+		"""
+	
+	
+	}
+    
+   
  }
   
 //   aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 555138969324.dkr.ecr.us-east-1.amazonaws.com  
