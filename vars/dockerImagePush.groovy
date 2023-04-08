ac342dbbc8b0e0ac81e05d3c27748f7b861cfd9e
Bhanu-Ganga-DevOPs <gangabhavanikatraparthi@gmail.com>
2023-04-08 06:38:01 +0000

    groovy changed

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,30 +1,30 @@
-// def call(String project, String ImageTag, String hubUser){
+ def call(String project, String ImageTag, String hubUser){
     
-//    withCredentials([usernamePassword(
-//     credentialsId: 'docker', 
-//     passwordVariable: 'PASS', 
-//     usernameVariable: 'USER'
-//     )]) {
+    withCredentials([usernamePassword(
+     credentialsId: 'docker', 
+    passwordVariable: 'PASS', 
+     usernameVariable: 'USER'
+     )]) {
         
-//         sh "docker login -u '$USER' -p '$PASS'"
-//     }
+         sh "docker login -u '$USER' -p '$PASS'"
+     }
 
-//     sh "docker image push ${hubUser}/${project}:${ImageTag}"
-//     sh "docker image push ${hubUser}/${project}:latest"
+     sh "docker image push ${hubUser}/${project}:${ImageTag}"
+     sh "docker image push ${hubUser}/${project}:latest"
+ }
+
+
+// def call(String aws_account_id, String region, String ecr_repoName){
+    
+//    sh """
+ //    export AWS_ACCESS_KEY_ID= 'AKIAYCQG4N3WL75AEINR'
+
+ //    export AWS_SECRET_ACCESS_KEY='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'
+
+ //    aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+    
+  //   docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+ //   """
 // }
-
-
-def call(String aws_account_id, String region, String ecr_repoName){
-    
-    sh """
-     export AWS_ACCESS_KEY_ID= 'AKIAYCQG4N3WL75AEINR'
-
-     export AWS_SECRET_ACCESS_KEY='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'
-
-     aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-    
-     docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
-    """
-}
   
 //   aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 555138969324.dkr.ecr.us-east-1.amazonaws.com  
