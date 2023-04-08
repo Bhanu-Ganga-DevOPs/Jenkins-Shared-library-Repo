0863c07a7e76cd9cc37284fd718f7d1ca8ecdfb2
Bhanu-Ganga-DevOPs <gangabhavanikatraparthi@gmail.com>
2023-04-08 07:24:55 +0000

    groovy changed

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -16,14 +16,16 @@
 
  def call(String aws_account_id, String region, String ecr_repoName){
  
-	
-    
-		 sh """
-
-		 aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
-    
+		 docker.withRegistry('https://555138969324.dkr.ecr.us-east-1.amazonaws.com/bhanuayikam', 'ecr:us-east-1:aws-creds') {
 		docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
-		"""
+  }
+    
+		// sh """
+
+		// aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.${region}.amazonaws.com
+    
+	//	docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+//		"""
 	
 	
 	
