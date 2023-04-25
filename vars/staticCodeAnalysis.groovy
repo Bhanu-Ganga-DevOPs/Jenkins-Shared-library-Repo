5eb5d2fc111c8a0b15ac279b5e96a180584ad889
Bhanu-Ganga-DevOPs <Kittu@225>
2023-04-25 01:44:01 -0400

    testing

--- vars/staticCodeAnalysis.groovy
+++ vars/staticCodeAnalysis.groovy
@@ -1,7 +1,10 @@
 def call(credentialsId){
-
     withSonarQubeEnv(credentialsId: credentialsId) {
         sh 'mvn clean package sonar:sonar'
     }
-    
 }
+
+
+
+
+//squ_d7dc3e329a073434310be8ec26cff486fcba72e6
