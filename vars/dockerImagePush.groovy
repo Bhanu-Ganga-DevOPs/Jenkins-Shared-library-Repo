5fe7b38ca429f488fad7503809f6abdeb809b54c
Bhanu-Ganga-DevOPs <Kittu@225>
2023-04-26 01:08:52 -0400

    testing

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -1,24 +1,24 @@
-// def call(String project, String ImageTag, String hubUser){
+ def call(String ImageName, String ImageTag, String DockerHubUser){
     
-  //  withCredentials([usernamePassword(
-    // credentialsId: 'docker', 
-  //  passwordVariable: 'PASS', 
-   //  usernameVariable: 'USER'
-    // )]) {
+    withCredentials([usernamePassword(
+        credentialsId: 'docker',
+        passwordVariable: 'PASS',
+        usernameVariable: 'USER'
+     )]) {
         
-      //   sh "docker login -u '$USER' -p '$PASS'"
-    // }
+         sh "docker login -u '$USER' -p '$PASS'"
+     }
 
-    // sh "docker image push ${hubUser}/${project}:${ImageTag}"
-   //  sh "docker image push ${hubUser}/${project}:latest"
- //}
+     sh "docker image push ${DockerHubUser}/${ImageName}:${ImageTag}"
+     sh "docker image push ${DockerHubUser}/${ImageName}:latest"
+ }
 
 
- def call(String aws_account_id, String region, String ecr_repoName){
- 
-		 docker.withRegistry('https://555138969324.dkr.ecr.us-east-1.amazonaws.com/bhanuayikam', 'ecr:us-east-1:aws-creds') {
-		docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
-  }
+// def call(String aws_account_id, String region, String ecr_repoName){
+//
+//		 docker.withRegistry('https://555138969324.dkr.ecr.us-east-1.amazonaws.com/bhanuayikam', 'ecr:us-east-1:aws-creds') {
+//		docker push ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest
+//  }
     
 		// sh """
 
@@ -29,8 +29,6 @@
 	
 	
 	
-    
-   
- }
+
   
 //   aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 555138969324.dkr.ecr.us-east-1.amazonaws.com  
